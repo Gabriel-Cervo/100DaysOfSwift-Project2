58fc862a182cfb2ccf0d530fc2e66d3893aa2ac9
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-25 09:57:00 -0300

    ADD bordas nas imagens

--- 100DaysOfSwift-Project2/ViewController.swift
+++ 100DaysOfSwift-Project2/ViewController.swift
@@ -20,6 +20,18 @@ class ViewController: UIViewController {
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
     
+        // layer = CALayer... Modificar views
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        // CALayer nao reconhece UIColor, entao é necessário chamar especificamente, e tambem é necessário passar um cgColor (CALayer nao reconhece UIColor...), chamamos então essa propriedade do UIColor
+        // Criar proprias cores:
+        // UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0)
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
         askQuestion()
     }
     
